3b94d4620fe28954db88ab6d2dd973d5946e122c
csy-59 <csy010509@naver.com>
2022-07-27 08:36:25 +0900

    [Feat] GameOver UI 무지개 색으로 반짝반짝

--- ChickenRun/Assets/Script/Manager/UIManager.cs
+++ ChickenRun/Assets/Script/Manager/UIManager.cs
@@ -13,10 +13,26 @@ public class UIManager : MonoBehaviour
     public Image ShapeImage;
     
     public GameObject GameOverUI;
+    public TextMeshProUGUI GameOverGameOverText;
+    public Image GameOverChickenImage;
     public TextMeshProUGUI GameOverUIScoreText;
     public TextMeshProUGUI GameOverUIBestScoreText;
 
+    public float ChangeSpeed = 1f;
+
     public TextMeshProUGUI FlowerCountText;
+
+    private int[] colorChangeTable = { 0, -1, 0, 0, 1, 0 };
+    private int currentChangePoint = 0; // R: 0  G: 4  B: 5
+    //private int[,] colorChangeTable =
+    //{
+    //    { 0, 1, 0 }, // 처음
+    //    { -1, 0, 0 }, //G가 1(255)가 된 후
+    //    { 0, 0, 1 }, //R이 0(0)이 된 후
+    //    { 0, -1, 0 }, //B가 1(255)가 된 후
+    //    { 1, 0, 0 }, //G가 0(0)가 된 후
+    //    { 0, 0, -1 } //R이 1(255)이 된 후
+    //};
 
     private void Start()
     {
@@ -31,6 +47,8 @@ public class UIManager : MonoBehaviour
 
         GameManager.Instance.OnGainFlower.RemoveListener(ChangeFlowerCount);
         GameManager.Instance.OnGainFlower.AddListener(ChangeFlowerCount);
+
+        GameOverGameOverText.color = new Color(1, 0, 0);
 
         if (!PlayerPrefs.HasKey("BestScore"))
         {
@@ -53,14 +71,15 @@ public class UIManager : MonoBehaviour
     {
         InGameUI.SetActive(false);
         GameOverUI.SetActive(true);
-        GameOverUIScoreText.text = $"Final Score\n{GameManager.Instance.score / 10}.{GameManager.Instance.score % 10}s";
+        StartCoroutine(GameOverEffect());
+        GameOverUIScoreText.text = $"Final Score : {GameManager.Instance.score / 10}.{GameManager.Instance.score % 10}s";
         int bestScore = PlayerPrefs.GetInt("BestScore");
         if (bestScore < GameManager.Instance.score)
         {
             bestScore = GameManager.Instance.score;
             PlayerPrefs.SetInt("BestScore", GameManager.Instance.score);
         }
-        GameOverUIBestScoreText.text = $"Best Score\n{bestScore / 10}.{bestScore % 10}s";
+        GameOverUIBestScoreText.text = $"Best Score : {bestScore / 10}.{bestScore % 10}s";
     }
 
     private void ShowShape()
@@ -87,5 +106,140 @@ public class UIManager : MonoBehaviour
     private void ChangeFlowerCount()
     {
         FlowerCountText.text = $"X {GameManager.Instance.FlowerCount}";
+    }
+
+    private IEnumerator GameOverEffect()
+    {
+        const int R = 0;
+        const int G = 1;
+        const int B = 2;
+        const int COLOR_MAX = 3;
+
+        int curColor = G;
+
+        float elapsedTime = 0;
+        // ChangeSpeed
+
+        float startValue = 1;
+        float endValue = 0;
+        float curValue;
+
+        while (true)
+        {
+            if (elapsedTime >= ChangeSpeed)
+            {
+                Color textColor = GameOverGameOverText.color;
+                switch (curColor)
+                {
+                    case R:
+                        textColor.r = endValue;
+                        break;
+                    case G:
+                        textColor.g = endValue;
+                        break;
+                    case B:
+                        textColor.b = endValue;
+                        break;
+                }
+                GameOverGameOverText.color = textColor;
+
+                switch (curColor)
+                {
+                    case R:
+                        if (textColor.r == 1f)
+                        {
+                            startValue = 1f;
+                            endValue = 0f;
+                        }
+                        else
+                        {
+                            startValue = 0f;
+                            endValue = 1f;
+                        }
+                        break;
+                    case G:
+                        if (textColor.g == 1f)
+                        {
+                            startValue = 1f;
+                            endValue = 0f;
+                        }
+                        else
+                        {
+                            startValue = 0f;
+                            endValue = 1f;
+                        }
+                        break;
+                    case B:
+                        if (textColor.b == 1f)
+                        {
+                            startValue = 1f;
+                            endValue = 0f;
+                        }
+                        else
+                        {
+                            startValue = 0f;
+                            endValue = 1f;
+                        }
+                        break;
+                }
+
+                curColor = (curColor + 1) % COLOR_MAX;
+            }
+            else
+            {
+                elapsedTime += Time.deltaTime;
+                curValue = Mathf.Lerp(startValue, endValue, elapsedTime / ChangeSpeed);
+                Color textColor = GameOverGameOverText.color;
+                switch (curColor)
+                {
+                    case R:
+                        textColor.r = curValue;
+                        break;
+                    case G:
+                        textColor.g = curValue;
+                        break;
+                    case B:
+                        textColor.b = curValue;
+                        break;
+                }
+                GameOverGameOverText.color = textColor;
+            }
+
+            //GameOverGameOverText.color = new Color(
+            //    GameOverGameOverText.color.r + colorChangeTable[currentChangePoint],
+            //    GameOverGameOverText.color.g + colorChangeTable[(currentChangePoint + 4) % 6],
+            //    GameOverGameOverText.color.b + colorChangeTable[(currentChangePoint + 5) % 6]
+            //);
+
+            //switch(currentChangePoint)
+            //{
+            //    case 0:
+            //        if (GameOverGameOverText.color.g >= 255)
+            //            currentChangePoint = (currentChangePoint + 1) % 6;
+            //        break;
+            //    case 1:
+            //        if (GameOverGameOverText.color.r <= 0)
+            //            currentChangePoint = (currentChangePoint + 1) % 6;
+            //        break;
+            //    case 2:
+            //        if (GameOverGameOverText.color.b >= 255)
+            //            currentChangePoint = (currentChangePoint + 1) % 6;
+            //        break;
+            //    case 3:
+            //        if (GameOverGameOverText.color.g <= 0)
+            //            currentChangePoint = (currentChangePoint + 1) % 6;
+            //        break;
+            //    case 4:
+            //        if (GameOverGameOverText.color.g >= 0)
+            //            currentChangePoint = (currentChangePoint + 1) % 6;
+            //        break;
+            //    case 5:
+            //        if (GameOverGameOverText.color.r >= 255)
+            //            currentChangePoint = (currentChangePoint + 1) % 6;
+            //        break;
+            //}
+
+            yield return null;
+        }
     }
 }
